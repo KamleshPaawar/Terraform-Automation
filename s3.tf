abf0f80160174acbabccc08c516a05477f3c903f
KamleshPaawar <157335198+KamleshPaawar@users.noreply.github.com>
2024-03-03 12:16:02 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,12 @@
 resource "aws_s3_bucket" "my-s3-tfs-bucket" {
   bucket_prefix = var.bucket_prefix
-  acl = var.acl
+
   
   tags = var.tags
+}
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my-s3-tfs-bucket.id
+  acl = var.acl
 }
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.my-s3-tfs-bucket.id
