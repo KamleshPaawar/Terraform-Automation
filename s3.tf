48b8b81a298a36cbad0891218c3f3d419db075e1
KamleshPaawar <157335198+KamleshPaawar@users.noreply.github.com>
2024-03-03 12:13:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,9 +2,11 @@ resource "aws_s3_bucket" "my-s3-tfs-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
-   versioning {
-    enabled = var.versioning
-  }
-  
   tags = var.tags
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my-s3-tfs-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
